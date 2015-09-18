c3234a9b820e6c9d23771c5923c1f2ea4a190154
jpeschke <j.peschke@syseleven.de>
2015-09-18 12:17:58 +0200

    add sample data

--- example-project/data/index.php
+++ example-project/data/index.php
@@ -11,4 +11,9 @@ echo "<br>";
 echo "Debug danach:".$_SESSION['tier'];
 echo "<br>";
 echo "Server:".$_SERVER["SERVER_ADDR"];
+
+$conn_string = "host=db0.local port=5432 dbname=syseleven user=syseleven password=syseleven_pass";
+$dbconn = pg_connect($conn_string);
+# so something usefull with that connection: store data on master server read from slave etc.
+
 ?>
