1c92e569dd083951f7983dd1740734aa9d4c1706
Jan Peschke <j.peschke@syseleven.de>
2016-09-29 10:21:27 +0000

    add database deployment

--- lampServer/scripts/install_lampstack.sh
+++ lampServer/scripts/install_lampstack.sh
@@ -9,12 +9,36 @@ until ping -c 1 syseleven.de; do sleep 5; done
 # install necessary services
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" haveged unzip wget jq git apache2 libapache2-mod-php mysql-server php7.0  
+apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" pwgen haveged unzip wget jq git apache2 libapache2-mod-php mysql-server php7.0  
+
+# creating a database
+rootpass=$(pwgen 16 1)
+customerpass=$(pwgen 16 1)
+/usr/bin/mysqladmin -u root password "$mypass"
+
+cat <<EOF> /root/.my.cnf
+[client]
+user = root
+password = ${mypass} 
+host = localhost
+EOF
+
+cat <<EOF> /root/createDB.sql
+CREATE DATABASE syseleven;
+CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON syseleven.* TO 'newuser'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
+sed -i "s/password/${customerpass}/g" /root/createDB.sql
+
+mysql < /root/createDB.sql
 
 # basic deployment of any app
 rm /var/www/html/*
 git clone https://gitlab.syseleven.de/j.peschke/anyapp.git /var/www/html/
 curl "https://raw.githubusercontent.com/syseleven/heattemplates-examples/master/lampServer/exampleApp/index.php > /var/www/html/index.php
+
 
 echo "finished generic lamp setup"
 
