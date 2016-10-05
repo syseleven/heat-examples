a4e9aeda98469d84462aab91fbc289112072f87e
Jan Peschke <j.peschke@syseleven.de>
2016-10-05 12:53:20 +0000

    add some useful php extensions

--- lampServer/scripts/install_lampstack.sh
+++ lampServer/scripts/install_lampstack.sh
@@ -9,7 +9,7 @@ until ping -c 1 syseleven.de; do sleep 5; done
 # install necessary services
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" pwgen haveged unzip wget jq git apache2 libapache2-mod-php mysql-server php7.0 php7.0-mysql 
+apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" pwgen haveged unzip wget jq git apache2 libapache2-mod-php mysql-server php7.0 php7.0-mysql php7.0-curl php7.0-intl php7.0-mbstring 
 
 # creating a database
 rootpass=$(pwgen 16 1)
