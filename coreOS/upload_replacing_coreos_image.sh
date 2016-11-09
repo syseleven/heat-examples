62630dca11306b137f7911ccb6d8c0e47d3c3946
Lars Fronius <l.fronius@googlemail.com>
2016-11-09 10:40:21 +0100

    Proposing making the uploaded CoreOS Version explicitly noted in Image Name

--- coreOS/upload_replacing_coreos_image.sh
+++ coreOS/upload_replacing_coreos_image.sh
@@ -3,16 +3,17 @@
 openstack image list --property name=private_coreos -f value | awk '{print $1}' | xargs openstack image delete
 
 # download and unpack latest release
+eval $(curl https://stable.release.core-os.net/amd64-usr/current/version.txt)
 curl https://stable.release.core-os.net/amd64-usr/current/coreos_production_openstack_image.img.bz2 | bzcat > coreos_production_openstack_image.img
 
 echo "Uploading image..."
 
 # upload image
-openstack image create private_coreos \
+openstack image create \
   --container-format bare \
   --disk-format qcow2 \
   --file coreos_production_openstack_image.img \
-  --private
+  --private "Private CoreOS $COREOS_VERSION"
 
 # delete temporary files
 rm coreos_production_openstack_image.img
