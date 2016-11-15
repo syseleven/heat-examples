c936e7dae771541f8e99fc1e4d824f3efb9c8da6
Jan Peschke <j.peschke@syseleven.de>
2016-11-15 16:11:12 +0000

    enable mod_rewrite, add php-gd and php-zip

--- lampServer/scripts/install_lampstack.sh
+++ lampServer/scripts/install_lampstack.sh
@@ -9,12 +9,13 @@ until ping -c 1 syseleven.de; do sleep 5; done
 # install necessary services
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" composer pwgen haveged unzip wget jq git apache2 libapache2-mod-php mysql-server php7.0 php7.0-mysql php7.0-curl php7.0-intl php7.0-mbstring php7.0-xml 
+apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" composer pwgen haveged unzip wget jq git apache2 libapache2-mod-php mysql-server php7.0 php7.0-mysql php7.0-curl php7.0-intl php7.0-mbstring php7.0-xml php7.0-gd php7.0-zip 
 
 phpenmod  mbstring xml intl curl mysqli mysqlnd 
 
 # disable unneeded apache modules
-a2dismod autoindex deflate status localized-error-pages serve-cgi-bin   
+a2dismod autoindex deflate status localized-error-pages serve-cgi-bin  
+a2enmod rewrite
 systemctl restart apache2
 
 # creating a database
