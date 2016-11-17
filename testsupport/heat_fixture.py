3391ed04d36541b7e812c37c1a9c77ace73f6cb8
Steffen Neubauer <s.neubauer@syseleven.de>
2016-11-17 10:57:52 +0100

    Allow testing the same stack multiple times in parallel

--- testsupport/heat_fixture.py
+++ testsupport/heat_fixture.py
@@ -1,18 +1,21 @@
 import random
 import string
-
 import pytest
-import testinfra.plugin
 
 
 def randomword(length):
     return ''.join(random.choice(string.lowercase) for _ in range(length))
 
 
-local_run_expect = testinfra.get_backend("local://").get_module("Command").run_expect
+def local_run_expect(*args, **kwargs):
+    import testinfra.plugin
+    local_run_expect = testinfra.get_backend("local://").get_module("Command").run_expect
+    return local_run_expect(*args, **kwargs)
+
 
 @pytest.fixture(scope="module")
 def HeatTemplate():
+    from openstack_backend import OpenstackBackend
     class Template:
         def __init__(self, yaml, **parameters):
             self.yaml = yaml
@@ -20,6 +23,9 @@ def HeatTemplate():
             self.stack_name = "testinfra_" + randomword(10)
 
         def create(self):
+            # TODO: find a cleaner solution
+            OpenstackBackend.stack_name = self.stack_name
+
             parameters_string = " ".join(
                 map(lambda key: "--parameter {key}={value}".format(key=key, value=self.parameters[key]),
                     self.parameters.keys()))
@@ -32,6 +38,9 @@ def HeatTemplate():
             local_run_expect([0], create_command)
 
         def destroy(self):
+            # TODO: find a cleaner solution
+            OpenstackBackend.stack_name = None
+
             delete_command = "openstack stack delete --yes {stack_name}".format(
                                     stack_name=self.stack_name
                                 )
