249a942edc1f89de59d0abace6d15e8eb2cd100a
dschwabeS11 <30408408+dschwabeS11@users.noreply.github.com>
2018-09-05 14:34:53 +0200

    add php and anyapp

--- two-regions-app-lb/scripts/install_lampstack.sh
+++ two-regions-app-lb/scripts/install_lampstack.sh
@@ -9,10 +9,9 @@ until ping -c 1 syseleven.de; do sleep 5; done
 # install necessary services
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" composer pwgen haveged unzip wget jq git apache2 libapache2-mod-php
-
-
-
+apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
+composer pwgen haveged unzip wget jq git apache2 libapache2-mod-php \
+php php-curl php-intl php-mbstring php-xml php-gd php-zip
 
 
 cat <<EOF> /var/www/html/index.html
